bf5b3d27e67b6361bf5c66419a34deb5aee6270c
li,xiaokun <leeli@iu.edu>
2023-03-14 10:32:45 -0400

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Layout from './Layout';
+import './pages/IndexPage'
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+
+  return (
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />}/>
+          <Route path="/login" element={<LoginPage />}/>
+        </Route>
+
+    </Routes>
+  );
+}
+
+export default App
